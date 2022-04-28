78e90946dde5f445111189e5ceb3995892013228
Cormac Dickson <cdickson@FIL.ION.UCL.AC.UK>
2022-04-28 20:36:58 +0100

    starrted decison trial

--- main_trial_schedule.js
+++ main_trial_schedule.js
@@ -4,7 +4,7 @@ var main_trial_schedule = [
     "P_perf": [0,1,0,1,0,0],
     "O1_perf": [1,0,0,0,0,1],
     "O2_perf": [0,1,0,0,1,0],
-    "dectype": [2,3],
+    "dectype": [1,3],
     "outcomeEngage": [0,1],
     "Porder" : [0,1,2,3]
   },
@@ -22,7 +22,7 @@ var main_trial_schedule = [
    "P_perf": [0,1,0,0,1,0],
    "O1_perf": [0,0,0,1,0,1],
    "O2_perf": [0,1,1,0,0,1],
-   "dectype": [2,3],
+   "dectype": [3,3],
    "outcomeEngage": [3,2],
    "Porder" : [3,2,1,0]
   },
@@ -31,7 +31,7 @@ var main_trial_schedule = [
     "P_perf": [0,1,1,0,0,0],
     "O1_perf": [1,0,0,1,0,0],
     "O2_perf": [0,0,0,0,0,0],
-    "dectype": [2,3],
+    "dectype": [4,3],
     "outcomeEngage": [2,3],
     "Porder" : [0,1,2,3]
   },
@@ -40,7 +40,7 @@ var main_trial_schedule = [
     "P_perf": [0,0,0,0,0,0],
     "O1_perf": [0,0,0,0,0,0],
     "O2_perf": [0,0,0,0,0,0],
-    "dectype": [2,3],
+    "dectype": [1,3],
     "outcomeEngage": [2,3],
     "Porder" : [1,0,3,2]
   }
