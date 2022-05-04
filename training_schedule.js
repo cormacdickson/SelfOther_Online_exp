a568320630094b9abe12e462ee6a18438822ab3b
Cormac Dickson <cdickson@FIL.ION.UCL.AC.UK>
2022-05-04 17:10:58 +0100

    ping

--- training_schedule.js
+++ training_schedule.js
@@ -2,17 +2,17 @@ var training_schedule = [
   {
     "correct_ans": "arrowright",
     "coh_D": [0,0,0,0],
-    "coh": [0.5,0,0,0]
+    "coh": [0.7,0,0,0]
   },
   {
     "correct_ans": "arrowleft",
-    "coh_D": [0,0,0,0],
-    "coh": [0.5,0,0,0]
+    "coh_D": [180,0,0,0],
+    "coh": [0.7,0,0,0]
   },
  {
    "correct_ans": "arrowright",
    "coh_D": [0,0,0,0],
-   "coh": [0.5,0,0,0]
+   "coh": [0.7,0,0,0]
   },
   {
     "correct_ans": "arrowright",
@@ -21,17 +21,17 @@ var training_schedule = [
   },
     {
       "correct_ans": "arrowleft",
-      "coh_D": [0,0,0,0],
-      "coh":[0.5,0,0,0]
+      "coh_D": [180,0,0,0],
+      "coh":[0.7,0,0,0]
   },
   {
     "correct_ans": "arrowright",
     "coh_D": [0,0,0,0],
-    "coh": [0.5,0,0,0]
+    "coh": [0.7,0,0,0]
   },
   {
     "correct_ans": "arrowleft",
-    "coh_D": [0,0,0,0],
-    "coh": [0.5,0,0,0]
+    "coh_D": [180,0,0,0],
+    "coh": [0.7,0,0,0]
   }
   ]
