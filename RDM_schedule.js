472f43994a9c4dd51ce67a9ea9690cef578f8fb7
cormacdickson <cormacdickson1@gmail.com>
2022-05-11 10:22:40 +0100

    working version minus working main schedule

--- RDM_schedule.js
+++ RDM_schedule.js
@@ -1,41 +1,34 @@
 var RDM_schedule = [
   {
-    "correct_ans": "arrowright",
-    "coh_D": [0,0,0,0],
-    "coh": [0.512,0,0,0]
+    "coh": [0.032,0,0,0]
   },
   {
-    "correct_ans": "arrowleft",
-    "coh_D": [180,0,0,0],
-    "coh": [0.512,0,0,0]
+    "coh": [0.032,0,0,0]
   },
  {
-   "correct_ans": "arrowright",
-   "coh_D": [0,0,0,0],
    "coh": [0.032,0,0,0]
   },
   {
-    "correct_ans": "arrowleft",
-    "coh_D": [180,0,0,0],
     "coh": [0.032,0,0,0]
+  },
+  {
+    "coh": [0.032,0,0,0]
+  },
+  {
+    "coh": [0.032,0,0,0]
+  },
+ {
+   "coh": [0.032,0,0,0]
+  },
+  {
+    "coh": [0.032,0,0,0]
+  },
+  {
+    "coh": [0.512,0,0,0]
+  },
+  {
+    "coh": [0.512,0,0,0]
   }
    ]
 
-   /*
-    {
-      "correct_ans": "arrowleft",
-      "coh_D": [180,0,0,0],
-      "coh":[0.7,0,0,0]
-  },
-  {
-    "correct_ans": "arrowright",
-    "coh_D": [0,0,0,0],
-    "coh": [0.7,0,0,0]
-  },
-  {
-    "correct_ans": "arrowleft",
-    "coh_D": [180,0,0,0],
-    "coh": [0.7,0,0,0]
-  }
- 
-*/
+  // 80:20 hard easy this array needs to be scaled up to the amount of trials we want
--- main_trial_schedule.js
+++ main_trial_schedule.js
@@ -6,7 +6,6 @@ var main_sch_1 = [
     "O2_perf": [0,1,0,0,1,0],
     "dectype": [1,3],
     "outcomeEngage": [2,1],
-    "Porder" : [0,1,2,3]
   },
   {
     "S_perf": [1,0,0,1,0,1],
@@ -15,7 +14,6 @@ var main_sch_1 = [
     "O2_perf": [0,0,1,0,1,0],
     "dectype": [2,3],
     "outcomeEngage": [1,-2],
-    "Porder" : [3,2,1,0]
   },
  {
    "S_perf": [0,1,0,0,1,0],
@@ -24,7 +22,6 @@ var main_sch_1 = [
    "O2_perf": [0,1,1,0,0,1],
    "dectype": [3,3],
    "outcomeEngage": [-3,2],
-   "Porder" : [0,1,2,3]
   },
   {
     "S_perf": [0,1,1,1,1,0],
@@ -33,7 +30,6 @@ var main_sch_1 = [
     "O2_perf": [0,0,0,0,0,0],
     "dectype": [4,3],
     "outcomeEngage": [2,3],
-    "Porder" : [3,2,1,0]
   },
   {
     "S_perf": [0,0,0,0,0,0],
@@ -42,6 +38,91 @@ var main_sch_1 = [
     "O2_perf": [0,0,0,0,0,0],
     "dectype": [1,3],
     "outcomeEngage": [2,-3],
-    "Porder" : [1,0,3,2]
+  }
+  ];
+
+  var main_sch_2 = [
+  {
+    "S_perf": [0,0,1,0,0,0],
+    "P_perf": [0,1,0,1,0,0],
+    "O1_perf": [1,0,0,0,0,1],
+    "O2_perf": [0,1,0,0,1,0],
+    "dectype": [1,3],
+    "outcomeEngage": [2,1],
+  },
+  {
+    "S_perf": [1,0,0,1,0,1],
+    "P_perf": [1,1,1,0,0,1],
+    "O1_perf": [0,1,0,1,0,1],
+    "O2_perf": [0,0,1,0,1,0],
+    "dectype": [2,3],
+    "outcomeEngage": [1,-2],
+  },
+ {
+   "S_perf": [0,1,0,0,1,0],
+   "P_perf": [0,1,0,0,1,0],
+   "O1_perf": [0,0,0,1,0,1],
+   "O2_perf": [0,1,1,0,0,1],
+   "dectype": [3,3],
+   "outcomeEngage": [-3,2],
+  },
+  {
+    "S_perf": [0,1,1,1,1,0],
+    "P_perf": [0,1,1,0,0,0],
+    "O1_perf": [1,0,0,1,0,0],
+    "O2_perf": [0,0,0,0,0,0],
+    "dectype": [4,3],
+    "outcomeEngage": [2,3],
+  },
+  {
+    "S_perf": [0,0,0,0,0,0],
+    "P_perf": [0,0,0,0,0,0],
+    "O1_perf": [0,0,0,0,0,0],
+    "O2_perf": [0,0,0,0,0,0],
+    "dectype": [1,3],
+    "outcomeEngage": [2,-3],
+  }
+  ];
+
+  var main_sch_3 = [
+  {
+    "S_perf": [0,0,1,0,0,0],
+    "P_perf": [0,1,0,1,0,0],
+    "O1_perf": [1,0,0,0,0,1],
+    "O2_perf": [0,1,0,0,1,0],
+    "dectype": [1,3],
+    "outcomeEngage": [2,1],
+  },
+  {
+    "S_perf": [1,0,0,1,0,1],
+    "P_perf": [1,1,1,0,0,1],
+    "O1_perf": [0,1,0,1,0,1],
+    "O2_perf": [0,0,1,0,1,0],
+    "dectype": [2,3],
+    "outcomeEngage": [1,-2],
+  },
+ {
+   "S_perf": [0,1,0,0,1,0],
+   "P_perf": [0,1,0,0,1,0],
+   "O1_perf": [0,0,0,1,0,1],
+   "O2_perf": [0,1,1,0,0,1],
+   "dectype": [3,3],
+   "outcomeEngage": [-3,2],
+  },
+  {
+    "S_perf": [0,1,1,1,1,0],
+    "P_perf": [0,1,1,0,0,0],
+    "O1_perf": [1,0,0,1,0,0],
+    "O2_perf": [0,0,0,0,0,0],
+    "dectype": [4,3],
+    "outcomeEngage": [2,3],
+  },
+  {
+    "S_perf": [0,0,0,0,0,0],
+    "P_perf": [0,0,0,0,0,0],
+    "O1_perf": [0,0,0,0,0,0],
+    "O2_perf": [0,0,0,0,0,0],
+    "dectype": [1,3],
+    "outcomeEngage": [2,-3],
   }
   ]
