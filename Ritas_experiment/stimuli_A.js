4bb239ebb0993cdf8fad6d09825a76f101fcab91
Cormac Dickson <cdickson@FIL.ION.UCL.AC.UK>
2022-05-12 15:34:31 +0100

    adding ritas exp to snd to her temp

---
+++ Ritas_experiment/stimuli_A.js
@@ -0,0 +1,502 @@
+var sequence_A =[
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/3.png",
+    "correct_response": 0,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Right', 'Left']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Right', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 2,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 2,
+    "options": ['Blank', 'Right', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 2,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 0,
+    "options": ['Right', 'Left', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Right', 'Left']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 0,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 0,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Right', 'Left']
+  },
+  {
+    "stimulus": "img/3.png",
+    "correct_response": 2,
+    "options": ['Right', 'Left', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 0,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/3.png",
+    "correct_response": 0,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 2,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 2,
+    "options": ['Blank', 'Right', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 0,
+    "options": ['Right', 'Left', 'Blank']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 0,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Right', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 2,
+    "options": ['Blank', 'Right', 'Left']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 2,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Right', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/3.png",
+    "correct_response": 1,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 0,
+    "options": ['Right', 'Left', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 2,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 0,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 2,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/3.png",
+    "correct_response": 2,
+    "options": ['Right', 'Left', 'Blank']
+  },
+  {
+    "stimulus": "img/3.png",
+    "correct_response": 0,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 2,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 2,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 2,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 2,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/3.png",
+    "correct_response": 1,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 2,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/3.png",
+    "correct_response": 2,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 0,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Right', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 2,
+    "options": ['Blank', 'Right', 'Left']
+  },
+  {
+    "stimulus": "img/3.png",
+    "correct_response": 0,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 0,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 0,
+    "options": ['Right', 'Left', 'Blank']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/3.png",
+    "correct_response": 1,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 0,
+    "options": ['Right', 'Left', 'Blank']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Right', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 2,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/3.png",
+    "correct_response": 2,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 2,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 2,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/3.png",
+    "correct_response": 2,
+    "options": ['Right', 'Left', 'Blank']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Right', 'Left']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 0,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 2,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/3.png",
+    "correct_response": 0,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 0,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 1,
+    "options": ['Right', 'Left', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 2,
+    "options": ['Right', 'Blank', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/3.png",
+    "correct_response": 0,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Blank', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 1,
+    "options": ['Right', 'Left', 'Blank']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Right', 'Blank']
+  },
+  {
+    "stimulus": "img/3.png",
+    "correct_response": 2,
+    "options": ['Right', 'Left', 'Blank']
+  },
+  {
+    "stimulus": "img/2.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Right', 'Left']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 1,
+    "options": ['Blank', 'Left', 'Right']
+  },
+  {
+    "stimulus": "img/1.png",
+    "correct_response": 0,
+    "options": ['Left', 'Blank', 'Right']
+  }
+]
