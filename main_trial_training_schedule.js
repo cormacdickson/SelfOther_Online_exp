74189776b39fc8de6cf6a46f6d84c19cfaf399e1
cormacdickson <cormacdickson1@gmail.com>
2022-06-01 11:48:28 +0100

    changes to poder sch notes

--- main_trial_training_schedule.js
+++ main_trial_training_schedule.js
@@ -1,5 +1,5 @@
 // variable ontaing the decisions and outcoe values we use during the training for the main experiment
-// NOTE: as of 26/5/22 these are not correct and need to be updated
+// NOTE: as of 26/5/22 these are not correct i.e they don't match the pppt and need to be updated
 var main_trial_training_schedule = [
   {
     "dectype": 1,
